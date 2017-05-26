3d24741767492dd2febe9c7b7a9b54e2d525537d
Jibran Kalia <jkalia@student.42.us.org>
2017-05-25 17:30:27 -0700

    Most heatmaps seem to be working

---
+++ p1run2.sh
@@ -0,0 +1,2 @@
+make reset
+./filler_vm -f maps/map02 -p1 ./filler -p2 players/abanlin.filler 1> crap
---
+++ p2run2.sh
@@ -0,0 +1,2 @@
+make reset
+./filler_vm -f maps/map02 -p1 players/abanlin.filler -p2 ./filler 1> crap
